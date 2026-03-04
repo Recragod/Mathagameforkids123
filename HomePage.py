c00417a3d20639d372f1f0a076800b94dc63d4a0
Recragod <joe.shikagishi@sydstu.catholic.edu.au>
2026-03-05 09:39:04 +1100

    4/03/26
    Trouble Shooting

--- HomePage.py
+++ HomePage.py
@@ -2,7 +2,7 @@
 #Flask create the web app
 
 from flask import Flask, render_template,request
-app - Flask(__name__)
+app = Flask(__name__)
 
 #variables for login details
 USERNAME = "admin"
@@ -14,11 +14,11 @@ PASSWORD = "1234"
 #name of function called home
 def home():
 #this shows the login.html page to the user
-    return render_template("LoginScreen.html")
+    return render_template("login.html")
 
 #This route runs when the login form is submitted
 #it only accepts POST requests - because the form uses POST in HTML
-@app.route("/LoginScreen", methods=["POST"])
+@app.route("/login", methods=["POST"])
 
 #name of function called login to accept data the user types
 def login():
@@ -30,10 +30,10 @@ def login():
     #Check if the username and password entered is a match to whats saved
     if username == USERNAME and password == PASSWORD:
         #if the correct details are entered, show success message
-        return "Log in successful"
+        return "Login successful"
     else:
         #if the incorrect details are entered, show invalid message
-        return "invalid username or password"
+        return "Invalid username or password"
 
 #This makes sure that the program runs only when this file is executed directly
 if __name__ == "__main__":
