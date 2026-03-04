b7a36e744e024429c946fa5363579d3d091e3a82
Recragod <joe.shikagishi@sydstu.catholic.edu.au>
2026-03-05 09:45:20 +1100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,3 @@
-console.log ("Login Successful")
+// This line writes the text "Login page loaded" to the browser's developer console.
+// It is mainly used for debugging to check that the JavaScript file is connected properly
+console.log ("Login Successful");
