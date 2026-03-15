37ac5c5de143f860519191484e480c4e233fe9e4
Recragod <joe.shikagishi@sydstu.catholic.edu.au>
2026-03-16 10:25:32 +1100

    16/03/26
    minor Tweaks

--- static/script.js
+++ static/script.js
@@ -2,9 +2,9 @@
 // It is mainly used for debugging to check that the JavaScript file is connected properly
 console.log ("Login page loaded");
 
-//---------
+//---------------------------------------------------------------
 //Function to open the signup page in a popup window
-//---------
+//---------------------------------------------------------------
 function openSignupWindow()
 {
     window.open('/signup', 'Singup', 'width=400,height=400,top=200,left=500');
