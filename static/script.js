a0779c9464db1e8e45df0d78ffbc880f51fae5ec
Recragod <joe.shikagishi@sydstu.catholic.edu.au>
2026-03-30 23:46:38 +1100

    Comments + neater format
    I added comments for the CSS  and JavaScript mainly. Additionally, I changed the format for the html comments while adding a few more in the process.
    This will ensure that the code can be read clearly with the comments explaining most of the lines. Also it allows consistency throughout other html files with similar codes

--- static/script.js
+++ static/script.js
@@ -10,17 +10,16 @@ function openSignupWindow()
     window.open('/signup', 'Signup', 'width=400,height=400,top=200,left=500');
 }
 
-
-    var count = 6000;
-    var counter = setInterval(timer, 10);
+    var count = 6000; // Sets the starting value for the countdown (6000 units of 10ms = 60 seconds)
+    var counter = setInterval(timer, 10); // Starts a repeating timer that runs the function every 10 milliseconds
     
-    function timer()
+    function timer() // Creates a function called timer
     {
-        if (count <= 0)
+        if (count <= 0) // This checks if the countdown has reached zero or less
         {
-            clearInterval(counter);
-            return;
+            clearInterval(counter); // This stops the interval from running further
+            return; // Exits the function
          }
-         count--;
-         document.getElementById("Timer").innerHTML = (count / 100).toFixed(2);
+         count--; // Subtracts 1 from the 'count' variable
+         document.getElementById("Timer").innerHTML = (count / 100).toFixed(2); // Calculates the seconds, formats it to 2 decimal places
      }
